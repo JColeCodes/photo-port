bce68bdd41e50d1ddad8ff67541c9de373fc683b
Jennifer Cole <capauldi@gmail.com>
2022-03-17 02:29:58 -0700

    added gallery page + states and hooks for the navigation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,6 +6,9 @@ function About() {
         <section className="my-5">
             <h1 id="about">Who am I?</h1>
             <img src={coverImage} className="my-2" style={{ width: "100%"}} alt="cover" />
+            <div className='my-2'>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem tempore necessitatibus aperiam debitis omnis quasi, quisquam perspiciatis eaque quidem deserunt repellendus ipsa soluta? Possimus ullam a sed velit. Placeat, incidunt.</p>
+            </div>
         </section>
     );
 }
