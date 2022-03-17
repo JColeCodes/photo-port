0156c3b34b1a3fc14f784a7052d9e0c8bf93948b
Jennifer Cole <capauldi@gmail.com>
2022-03-17 16:26:57 -0700

    added conditional rendering for contact

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,9 @@ function Nav(props) {
     const {
         categories = [],
         setCurrentCategory,
-        currentCategory
+        currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
     useEffect(() => {
@@ -22,14 +24,17 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about" data-testid="about">About me</a>
+                        <a href="#about" data-testid="about" onClick={() => setContactSelected(false)}>About me</a>
                     </li>
-                    <li>
-                        <span className="mx-2">Contact</span>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)} className="mx-2">Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
-                            <span onClick={() => setCurrentCategory(category)}>
+                        <li className={`mx-1 ${currentCategory.name === category.name && !contactSelected && 'navActive'}`} key={category.name}>
+                            <span onClick={() => {
+                                setCurrentCategory(category);
+                                setContactSelected(false);
+                            }}>
                                 {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
